ce2b7f779f7df1c6e833441625987f462b969cb1
Sophia Z <szhou0202@gmail.com>
2025-03-14 01:42:23 -0400

    got basic rudimentary flow working for user and buttery, minus some features

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ from flask import Flask, request, make_response, redirect, url_for, render_templ
 
 # ! should there be 2 separate apps for user and buttery
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__, template_folder='./html')
 
 # ! these should be in our eventual SQL DB
 USERNAME_PASSWORD_DB = {"username": "password"}
-BUTTERY_MENU_DB = {"Davenport" : ["Quesadilla 1", "Quesadilla 2"]}
+BUTTERY_PASSWORD_DB = {"Davenport": "Dive", "Morse": "Morsebutt", "Stiles": "Stilesbutt"}
+BUTTERY_MENU_DB = {"Davenport" : ["Quesadilla 1", "Quesadilla 2"],
+                   "Morse" : [],
+                   "Stiles" : []}
+# ! this needs to have a buttery identifier
 MENU_ITEM_DB = {"Quesadilla 1" : ["Cheese", "Flour tortilla"], "Quesadilla 2" : ["Flour tortilla"]}
 
 # we can probably store things like cart items and orders in progress in variables
@@ -26,7 +30,19 @@ def page_not_found(error):
 
 #-----------------------------------------------------------------------
 
+@app.route('/', methods=['GET'])
+def home():
+    html = render_template('index.html')
+    response = make_response(html)
+
+    return response
+
+#-----------------------------------------------------------------------
+# USER SIDE
+#-----------------------------------------------------------------------
+
 # ! apparently there is a Flask-Login extension that can handle this?
+# change this in future
 @app.route('/u_login', methods=['GET'])
 def u_login():
 
@@ -87,8 +103,7 @@ def u_login_submit():
     response.set_cookie('username', username)  # apparently this is how to do this?
     response.set_cookie('password', password)
 
-    global USERNAME
-    global PASSWORD
+    global USERNAME, PASSWORD
     USERNAME = username
     PASSWORD = password
 
@@ -98,16 +113,18 @@ def u_login_submit():
 
 @app.route('/u_butteries', methods=['GET'])  
 def u_butteries():
-    html = render_template('u_butteries.html')
+    html = render_template('u_butteries.html', butteries=BUTTERY_MENU_DB.keys())
     response = make_response(html)
 
     return response
+
 #-----------------------------------------------------------------------
 
-# ! should take in as argument which buttery, but we are just doing DPORT for now
 @app.route('/u_get_buttery_menu', methods=['GET'])  
 def u_get_buttery_menu():
-    html = render_template('u_butteryMenu.html', menuItems=BUTTERY_MENU_DB["Davenport"])
+    buttery = request.args.get("buttery")
+    html = render_template('u_butteryMenu.html', buttery=buttery,
+                           menuItems=BUTTERY_MENU_DB[buttery])
     response = make_response(html)
 
     return response
@@ -116,8 +133,10 @@ def u_get_buttery_menu():
 
 @app.route('/u_display_item', methods=['GET'])  # ! or should we make the link have the menu item ID
 def u_display_item():
+    buttery = request.args.get('buttery')
     menu_item = request.args.get('menu_item')
     html = render_template('u_menuItem.html', ingredients=MENU_ITEM_DB[menu_item],
+                           buttery=buttery,
                            menuItem=menu_item)
     response = make_response(html)
     
@@ -147,5 +166,131 @@ def u_cart():
 def u_account():
     html = render_template('u_account.html', username=USERNAME)
     response = make_response(html)
+    
+    return response
+
+#-----------------------------------------------------------------------
+# BUTTERY SIDE
+#-----------------------------------------------------------------------
+
+@app.route('/b_login', methods=['GET'])
+def b_login():
+
+    error_msg = request.cookies.get('error_msg')
+    if error_msg is None:
+        error_msg = ''
+
+    # remember the user
+    username = request.cookies.get('username')
+    if username is None:
+        username = ''
+
+    password = request.cookies.get('password')
+    if password is None:
+        password = ''
+
+    html = render_template('b_login.html', error_msg=error_msg,
+                           username=username,
+                           password=password)
+    response = make_response(html)
+
+    response.set_cookie('username', username)
+    response.set_cookie('password', password)
+    return response
+
+#-----------------------------------------------------------------------
+
+@app.route('/b_login_submit', methods=['GET'])  
+def b_login_submit():
+
+    username = request.args.get('uname')
+    password = request.args.get('pword')
+
+    username = username if username else ""
+    password = password if password else ""
+
+    if (username.strip() == '' or password.strip() == ''):
+        response = redirect(url_for('b_login'))
+        error_msg = 'Username or password empty.'
+        response.set_cookie('error_msg', error_msg)
+
+    elif username not in BUTTERY_PASSWORD_DB:
+        response = redirect(url_for('b_login'))
+        error_msg = 'Username not found. Create an account.'
+        response.set_cookie('error_msg', error_msg)
+
+    elif BUTTERY_PASSWORD_DB[username] != password:
+        response = redirect(url_for('b_login'))
+        error_msg = 'Password incorrect. You get [] more tries.'
+        response.set_cookie('error_msg', error_msg)
+
+    else:
+        html = redirect(url_for('b_myButtery'))
+
+        response = make_response(html)
+        response.set_cookie('error_msg', '')
+
+    response.set_cookie('username', username) 
+    response.set_cookie('password', password)
+
+    global USERNAME
+    global PASSWORD
+    USERNAME = username
+    PASSWORD = password
+
+    return response
+
+#-----------------------------------------------------------------------
+
+@app.route('/b_myButtery', methods=['GET'])  
+def b_myButtery():
+    html = render_template('b_myButtery.html', buttery=USERNAME,
+                           menuItems=BUTTERY_MENU_DB[USERNAME])  # ! kind of janky way to do it
+    response = make_response(html)
+
+    return response
+
+#-----------------------------------------------------------------------
+
+@app.route('/b_display_item', methods=['GET'])  # ! or should we make the link have the menu item ID
+def b_display_item():
+    menu_item = request.args.get('menu_item')
+    html = render_template('b_menuItem.html', ingredients=MENU_ITEM_DB[menu_item],
+                           buttery=USERNAME,
+                           menuItem=menu_item)
+    response = make_response(html)
+    
+    return response
+
+#-----------------------------------------------------------------------
+
+@app.route('/b_orderQueue', methods=['GET']) 
+def b_orderQeue():
+    html = render_template('b_orderQueue.html')
+    response = make_response(html)
+    
+    return response
+
+#-----------------------------------------------------------------------
+
+@app.route('/b_account', methods=['GET']) 
+def b_account():
+    html = render_template('b_account.html', username=USERNAME)  # ! change all instances of USERNAME to request.cookies.get('username') ?
+    response = make_response(html)
+    
+    return response
+#-----------------------------------------------------------------------
+
+@app.route('/logout', methods=['GET'])  # ! should we separate user and buttery logout
+def logout():
+    # clear vars
+    global USERNAME, PASSWORD
+    USERNAME = ''
+    PASSWORD = ''
+
+    html = redirect(url_for("home"))
+    response = make_response(html)
+    response.set_cookie('username', '')
+    response.set_cookie('password', '')
     
     return response
